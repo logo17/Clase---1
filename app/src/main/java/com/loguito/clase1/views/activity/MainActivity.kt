2761ffa5cfaeaf03bd02b631cb09a4feba8973e9
Heriberto <burenam17@gmail.com>
2021-05-29 11:17:18 -0600

    First commit
    Signed-off-by: Heriberto <burenam17@gmail.com>

---
+++ app/src/main/java/com/loguito/clase1/views/activity/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.loguito.clase1.views.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.loguito.clase1.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main_rl) {
+
+}
